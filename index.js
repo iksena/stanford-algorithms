29aa23af393c80f57532cb464973ee0e8eeb4e63
I Komang Sena <i.buwana@btpn.com>
2022-02-13 10:33:58 +0700

    Add eslint

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const { merge } = require('./inversions');
 
 console.log(merge([1,4,5,9,11,12,20,23,25,26], [2,3,6,7,8,10,13,21,22,24]))
+console.log(merge([1,3,4], [2]))
--- inversions.js
+++ inversions.js
@@ -1,22 +1,21 @@
 const merge = (leftArray, rightArray) => {
-    let i = 0;
-    let j = 0;
-    let splitInversions = 0;
-    let mergedArray = []
+  let i = 0;
+  let j = 0;
+  let splitInversions = 0;
+  const mergedArray = [];
 
-    for(let k = 0; k < leftArray.length + rightArray.length; k++) {
-        console.log(leftArray[i], rightArray[j]);
-        if(leftArray[i] < rightArray[j] || !rightArray[j]){
-            mergedArray[k] = leftArray[i];
-            i++;
-        } else {
-            mergedArray[k] = rightArray[j];
-            j++;
-            splitInversions = splitInversions + (leftArray.length - i)
-        }
+  for (let k = 0; k < leftArray.length + rightArray.length; k++) {
+    if (leftArray[i] < rightArray[j] || !rightArray[j]) {
+      mergedArray[k] = leftArray[i];
+      i++;
+    } else {
+      mergedArray[k] = rightArray[j];
+      j++;
+      splitInversions += (leftArray.length - i);
     }
+  }
 
-    return { mergedArray, splitInversions }
-}
+  return { mergedArray, splitInversions };
+};
 
-module.exports = { merge }
+module.exports = { merge };
