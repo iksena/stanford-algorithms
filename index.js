f9de39238e8234d4d13785d59697849ad9cbf940
I Komang Sena <i.buwana@btpn.com>
2022-02-13 13:16:06 +0700

    refactor

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const { mergeSort } = require('./inversions');
 
-const file = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
-const input = file.split(/\r?\n/).map((number) => Number(number));
+const assignment2 = () => {
+  const file = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+  const input = file.split(/\r?\n/).map((number) => Number(number));
 
-const [sortedArray, inversions] = mergeSort(input);
+  const [sortedArray, inversions] = mergeSort(input);
 
-console.log('Inversions: ', inversions);
+  console.log('Inversions: ', inversions);
+  console.log('Sample check', sortedArray[99999]);
+  console.log('Max number', Math.max(...input));
+};
 
-console.log('Sample check', sortedArray[99999]);
-console.log('Max number', Math.max(...input));
+assignment2();
