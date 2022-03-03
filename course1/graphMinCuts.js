23731998a7ac54cff63447db8d993431e7884505
I Komang Sena <i.buwana@btpn.com>
2022-03-03 16:31:24 +0700

    Implement kargerMinCut

--- course1/graphMinCuts.js
+++ course1/graphMinCuts.js
@@ -29,16 +29,59 @@ const constraintEdge = (graph, tail, head) => {
   );
   graph.delete(head);
   graph.forEach((edges, node) => {
+    // replace merged edge with paralel edge
     graph.set(node, edges.map((edge) => (edge === head ? tail : edge)));
   });
 
   return graph;
 };
 
-const kargerMinCut = (graph) => {
+const pickRandomEdge = (graph) => {
+  const nodes = [...graph.keys()];
+  const nodeIndex = Math.floor(Math.random() * (nodes.length - 1));
+  const head = nodes[nodeIndex];
 
+  const tails = graph.get(head);
+  const tailIndex = Math.floor(Math.random() * (tails.length - 1));
+  const tail = tails[tailIndex];
+
+  return [tail, head];
 };
 
-// console.log(readGraph('kargerMinCut.txt'));
+/**
+ * Find cuts with karger algorithm with 1 iteration
+ * @param {Map} graph - input graph
+ * @returns {number} remaining graph edges
+ */
+const findCuts = (graph) => {
+  while (graph.size > 2) {
+    const [tail, head] = pickRandomEdge(graph);
+    constraintEdge(graph, tail, head);
+  }
 
-module.exports = { readGraph, constraintEdge };
+  return graph.values().next().value.length;
+};
+
+/**
+ * Karger minimum cuts algorithm
+ * @param {Map} graph - input graph
+ * @returns {Map} merged edges graph
+ */
+const kargerMinCut = (graph) => {
+  const n = graph.size;
+  const nChoose2 = (n * (n - 1)) / 2;
+
+  let minCut = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < nChoose2; i++) {
+    const cut = findCuts(new Map(graph));
+    if (cut <= minCut) {
+      minCut = cut;
+    }
+  }
+
+  return minCut;
+};
+
+module.exports = {
+  readGraph, constraintEdge, pickRandomEdge, findCuts, kargerMinCut,
+};
--- course1/graphMinCuts.test.js
+++ course1/graphMinCuts.test.js
@@ -1,16 +1,26 @@
-const { readGraph, constraintEdge } = require('./graphMinCuts');
+const {
+  readGraph, findCuts, kargerMinCut,
+} = require('./graphMinCuts');
 const { readInput } = require('.');
 
 describe('Graph Min Cuts', () => {
   const runTestCase = (testCase) => () => {
     const graph = readGraph(`assignment4MinCut/input_random_${testCase}.txt`, ' ');
-    const [result] = readInput(`assignment4MinCut/output_random_${testCase}.txt`);
+    const [expectedMinCut] = readInput(`assignment4MinCut/output_random_${testCase}.txt`);
 
-    console.log(graph);
-    console.log(constraintEdge(graph, 2, 1));
+    const minCut = kargerMinCut(graph);
+
+    expect(minCut).toBe(expectedMinCut);
   };
 
   describe('#kargerMinCut', () => {
-    it('should return correct min cut for 1_6', runTestCase('1_6'));
+    it('should return correct min cut for 3_6', runTestCase('3_6'));
+    it('should return correct min cut for 8_10', runTestCase('8_10'));
+    it('should return correct min cut for 15_50', runTestCase('15_50'));
+    // it('should return correct min cut for 19_75', runTestCase('19_75'));
+    // it('should return correct min cut for 24_100', runTestCase('24_100'));
+    // it('should return correct min cut for 25_125', runTestCase('25_125'));
+    // it('should return correct min cut for 31_150', runTestCase('31_150'));
+    // it('should return correct min cut for 38_200', runTestCase('38_200'));
   });
 });
