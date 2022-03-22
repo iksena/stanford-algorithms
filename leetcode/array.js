5863e1a9ff316b70e0b46f578baee18655659546
I Komang Sena <i.buwana@btpn.com>
2022-03-22 10:29:45 +0700

    matrixRotationProblem

--- leetcode/array.js
+++ leetcode/array.js
@@ -312,4 +312,33 @@ const validSudokuMatrix = () => {
     ['.', '.', '.', '.', '8', '.', '.', '7', '9']]));
 };
 
-validSudokuMatrix();
+const matrixRotationProblem = () => {
+  const matrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  const rotateMatrix = (matrix) => {
+    if (matrix.length <= 1) {
+      return matrix;
+    }
+
+    for (let i = 0; i < matrix.length - 1; i++) {
+      for (let j = i + 1; j < matrix[i].length; j++) {
+        ([matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]);
+      }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i].reverse();
+    }
+
+    return matrix;
+  };
+
+  console.log(rotateMatrix(matrix1));
+  console.log(rotateMatrix([[1, 2], [3, 4]]));
+  console.log(rotateMatrix([[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]));
+};
+
+matrixRotationProblem();
