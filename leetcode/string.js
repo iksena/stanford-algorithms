6a1f21977361b6eee79e7a874a18e37962a9b4b5
I Komang Sena <i.buwana@btpn.com>
2022-03-24 09:27:52 +0700

    add reverseInteger

--- leetcode/string.js
+++ leetcode/string.js
@@ -11,7 +11,44 @@ const reverseString = () => {
     return s;
   };
 
+  const reverseGood = (s) => {
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+      [s[i], s[j]] = [s[j], s[i]];
+      i++;
+      j--;
+    }
+
+    return s;
+  };
+
   console.log(reverse(q));
 };
 
-reverseString();
+const reverseInteger = () => {
+  const q = -123;
+
+  const reverseInt = (x) => {
+    const range = 2147483648;
+    const sign = x < 0 ? -1 : 1;
+    const s = [...Math.abs(x).toString()];
+    let i = 0;
+    let j = s.length - 1;
+    while (i < j) {
+      [s[i], s[j]] = [s[j], s[i]];
+      i++;
+      j--;
+    }
+
+    const result = Number(s.join('')) * sign;
+    if (result < (-1 * range) || result > range - 1) return 0;
+
+    return result;
+  };
+
+  console.log(reverseInt(q));
+  console.log(reverseInt(1534236469));
+};
+
+reverseInteger();
